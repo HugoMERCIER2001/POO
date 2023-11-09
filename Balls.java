5bc4da0bd77e89482e1d24e749c763de9d994025
Hugo <hugo.mercier1@grenoble-inp.org>
2023-11-09 14:39:45 +0100

    nouveau push

--- Balls.java
+++ Balls.java
@@ -2,8 +2,8 @@ import java.awt.Point;
 
 public class Balls{
     private int nbBalls;
-    private Point[] listeBalls;
-    private Point[] listeBallsInit;
+    private Point[] listeBalls; //liste des points sur lesquels on fait des modifications (translater, déplacer etc ...).
+    private Point[] listeBallsInit; //liste des positions initiales des points.
 
 
     public int getNbBalls(){
@@ -30,10 +30,32 @@ public class Balls{
         }
     }
 
-    public void ajouterPoint(Point p){
-        //fonction qui ajoute un Point dans la classe (modifie donc nbBalls, listeBalls et listeBallsInit), à la fin des tableaux.
-        this.nbBalls = this.nbBalls + 1;
-        
-
+    public void initiePoint(Point p, int i){
+        //fonction qui initie le Point d'indice i la classe (modifie listeBalls ET listeBallsInit).
+        if(i >= this.nbBalls){
+            throw new IllegalArgumentException("L'indice dépasse la taille du tableau listeBalls")
+        }
+        else{
+        this.listeBalls[i] = p;
+        this.listeBallsInit[i] = p;
+        }
     }
-}
+
+    public Balls(int nbBalls){
+        //Constructeur de la classe qui crée un objet Balls dont tous les éléments de listeBalls et listeBallsInit sont à (0, 0).
+        this.nbBalls = nbBalls;
+        this.listeBalls = Point[nbBalls];
+        this.listeBallsInit = Point[nbBalls];
+        for(int i = 0; i <= nbBalls; i ++){
+            listeBalls[i] = new Point();
+            listeBallsInit[i] = new Point();
+        }
+    }
+
+    public Balls(Point[] listePoints){
+        //Constructeur de la classe qui crée un objet Balls dont la liste de Points est la liste donnée en argument.
+        this.nbBalls = listePoints.length;
+        this.listeBalls = listePoints;
+        this.listeBallsInit = listePoints;
+    }
+}²
